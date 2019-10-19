47f45b2a1054cba31b7e8ebda555a7225e8eeb80
Joosep J????ger <joosep.jaager@gmail.com>
2019-10-19 17:37:58 +0300

    Implemented some operations with scalars and AF division.

--- src/AffineForm.hs
+++ src/AffineForm.hs
@@ -2,10 +2,11 @@ module AffineForm where
 
 import Control.Monad.State
 
-import Numeric.Interval
+import Numeric.Interval hiding (midpoint)
 
 data AffineForm a =
   AffineForm a [a] a
+  deriving (Show)
 
 type AFIndex = Int
 type AFM = State AFIndex
@@ -19,7 +20,37 @@ newEps = do
   put $ idx + 1
   return $ AffineForm 0 (replicate idx 0 ++ [1]) 0
 
+-- Returns range without accounting for the error term
+range_ :: Num a => AffineForm a -> AFM a
+range_ (AffineForm _ xs _) = return . sum $ abs <$> xs
+
+range :: Num a => AffineForm a -> AFM a
+range af@(AffineForm _ _ xe) = (xe+) <$> range_ af
+
+midpoint :: Num a => AffineForm a -> AFM a
+midpoint (AffineForm x _ _) = return x
+
+lo :: Num a => AffineForm a -> AFM a
+lo af = do
+  r <- range af
+  c <- midpoint af
+  return $ r-c
+
+hi :: Num a => AffineForm a -> AFM a
+hi af = do
+  r <- range af
+  c <- midpoint af
+  return $ r+c
+
 -- Affine arithmetic operations
+
+-- Add scalar to midpoint
+(^+.) :: Num a => AffineForm a -> a -> AFM (AffineForm a)
+(AffineForm x xs xe) ^+. a = return $ AffineForm (x+a) xs xe
+
+-- Add scalar to error term
+(^+~) :: Num a => AffineForm a -> a -> AFM (AffineForm a)
+(AffineForm x xs xe) ^+~ e = return $ AffineForm x xs (xe+e)
 
 (^+^) :: Num a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
 (AffineForm x xs xe) ^+^ (AffineForm y ys ye) = return $ AffineForm (x+y) zs (xe+ye)
@@ -34,13 +65,24 @@ x ^-^ y = negateAF y >>= (x ^+^)
 (^*^) :: Num a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
 (AffineForm x xs xe) ^*^ (AffineForm y ys ye) = return $ AffineForm (x*y) zs ze
   where zs = (\(l,r) -> l+r) <$> zipWithZeros ((y*) <$> xs) ((x*) <$> ys)
-        ze = sum $ liftM2 (*) xs ys
+        ze = sum $ liftM2 (*) (xs ++ [xe]) (ys ++ [ye])
 
-recipAF :: Num a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
-recipAF (AffineForm y ys ye) = undefined
+(*^) :: Num a => a -> AffineForm a -> AFM (AffineForm a)
+a *^ (AffineForm x xs xe) = return $ AffineForm (a*x) ((a*) <$> xs) xe
 
-(^/^) :: Num a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
-(AffineForm x xs xe) ^/^ (AffineForm y ys ye) = undefined
+recipAF :: Fractional a => AffineForm a -> AFM (AffineForm a)
+recipAF af = do
+  high <- hi af
+  low <- lo af
+  let p = -1/high^^2
+      q = -p*(low+high)^^2/(2*low)
+      delta = -p*(low-high)^^2/(2*low)
+  scl <- p *^ af
+  shft <- scl ^+. q
+  shft ^+~ delta
+
+(^/^) :: Fractional a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
+x ^/^ y = (x ^*^) =<< (recipAF y)
 
 (^**^) :: Num a => AffineForm a -> a -> AFM (AffineForm a)
 (AffineForm x xs xe) ^**^ y = undefined
