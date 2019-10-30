859c0054d3264de626f425b2caf5f5b8e5f35b35
Joosep Jääger <joosep.jaager@gmail.com>
2019-10-30 18:45:34 +0200

    Implemented reciprocal. Replaced AFM types with type variables in signatures.

--- src/AffineForm.hs
+++ src/AffineForm.hs
@@ -2,10 +2,12 @@ module AffineForm where
 
 import Control.Monad.State
 
-import Numeric.Interval hiding (midpoint)
+import qualified Numeric.Interval as IA
+import Numeric.Interval ((...))
 
-data AffineForm a =
-  AffineForm a [a] a
+data AffineForm a
+  = AffineForm a [a] a
+  | Reals
   deriving (Show)
 
 type AFIndex = Int
@@ -14,78 +16,107 @@ type AFM = State AFIndex
 newState :: AFM ()
 newState = put 0
 
+eval :: AFM a -> a
+eval x = fst $ runState x 0
+
 newEps :: Num a => AFM (AffineForm a)
 newEps = do
   idx <- get
   put $ idx + 1
   return $ AffineForm 0 (replicate idx 0 ++ [1]) 0
 
+newFromInterval :: Fractional a => IA.Interval a -> AFM (AffineForm a)
+newFromInterval i = do
+  eps <- newEps
+  mult <- ((IA.width i) / 2) *^ eps
+  (IA.midpoint i) +^ mult
+
 -- Returns range without accounting for the error term
-range_ :: Num a => AffineForm a -> AFM a
+range_ :: (Num a, Monad m) => AffineForm a -> m a
 range_ (AffineForm _ xs _) = return . sum $ abs <$> xs
 
-range :: Num a => AffineForm a -> AFM a
+range :: (Num a, Monad m) => AffineForm a -> m a
 range af@(AffineForm _ _ xe) = (xe+) <$> range_ af
 
-midpoint :: Num a => AffineForm a -> AFM a
+midpoint :: (Num a, Monad m) => AffineForm a -> m a
 midpoint (AffineForm x _ _) = return x
+midpoint Reals = error "Reals has no midpoint"
 
-lo :: Num a => AffineForm a -> AFM a
+lo :: (Num a, Monad m) => AffineForm a -> m a
 lo af = do
   r <- range af
   c <- midpoint af
   return $ r-c
 
-hi :: Num a => AffineForm a -> AFM a
+hi :: (Num a, Monad m) => AffineForm a -> m a
 hi af = do
   r <- range af
   c <- midpoint af
   return $ r+c
 
+interval :: (Num a, Ord a, Monad m) => AffineForm a -> m (IA.Interval a)
+interval af = do
+  a <- lo af
+  b <- hi af
+  return (a...b)
+
 -- Affine arithmetic operations
 
--- Add scalar to midpoint
-(^+.) :: Num a => AffineForm a -> a -> AFM (AffineForm a)
-(AffineForm x xs xe) ^+. a = return $ AffineForm (x+a) xs xe
+-- Add scalar
+(+^) :: (Num a, Monad m) => a -> AffineForm a -> m (AffineForm a)
+a +^ (AffineForm x xs xe) = return $ AffineForm (x+a) xs xe
+_ +^ Reals = return Reals
 
 -- Add scalar to error term
-(^+~) :: Num a => AffineForm a -> a -> AFM (AffineForm a)
+(^+~) :: (Num a, Monad m) => AffineForm a -> a -> m (AffineForm a)
 (AffineForm x xs xe) ^+~ e = return $ AffineForm x xs (xe+e)
+Reals ^+~ _ = return Reals
 
-(^+^) :: Num a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
+(^+^) :: (Num a, Monad m) => AffineForm a -> AffineForm a -> m (AffineForm a)
 (AffineForm x xs xe) ^+^ (AffineForm y ys ye) = return $ AffineForm (x+y) zs (xe+ye)
   where zs = (\(l,r) -> l+r) <$> zipWithZeros xs ys
 
-negateAF :: Num a => AffineForm a -> AFM (AffineForm a)
+negateAF :: (Num a, Monad m) => AffineForm a -> m (AffineForm a)
 negateAF (AffineForm x xs xe) = return $ AffineForm (-x) (negate <$> xs) xe
 
-(^-^) :: Num a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
+(^-^) :: (Num a, Monad m) => AffineForm a -> AffineForm a -> m (AffineForm a)
 x ^-^ y = negateAF y >>= (x ^+^)
 
-(^*^) :: Num a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
+(^*^) :: (Num a, Monad m) => AffineForm a -> AffineForm a -> m (AffineForm a)
 (AffineForm x xs xe) ^*^ (AffineForm y ys ye) = return $ AffineForm (x*y) zs ze
   where zs = (\(l,r) -> l+r) <$> zipWithZeros ((y*) <$> xs) ((x*) <$> ys)
         ze = sum $ liftM2 (*) (xs ++ [xe]) (ys ++ [ye])
 
-(*^) :: Num a => a -> AffineForm a -> AFM (AffineForm a)
+(*^) :: (Num a, Monad m) => a -> AffineForm a -> m (AffineForm a)
 a *^ (AffineForm x xs xe) = return $ AffineForm (a*x) ((a*) <$> xs) xe
 
-recipAF :: Fractional a => AffineForm a -> AFM (AffineForm a)
+recipAF :: (Ord a, Fractional a, Monad m) => AffineForm a -> m (AffineForm a)
 recipAF af = do
   high <- hi af
-  low <- lo af
-  let p = -1/high^^2
-      q = -p*(low+high)^^2/(2*low)
-      delta = -p*(low-high)^^2/(2*low)
-  scl <- p *^ af
-  shft <- scl ^+. q
-  shft ^+~ delta
+  low  <- lo af
+  -- Any way to get rid of the if-else statements?
+  if low > 0
+    then recipAF' af
+    else if high < 0
+      then negateAF =<< recipAF' =<< negateAF af
+      else return Reals
 
-(^/^) :: Fractional a => AffineForm a -> AffineForm a -> AFM (AffineForm a)
+recipAF' :: (Ord a, Fractional a, Monad m) => AffineForm a -> m (AffineForm a)
+recipAF' af = do
+  a <- hi af
+  b <- lo af
+  let p = -1/b^2
+      q = -p*(a+b)^2/(2*a)
+      d = -p*(a-b)^2/(2*a)
+  mult <- p *^ af
+  shft <- q +^ mult
+  shft ^+~ d
+
+(^/^) :: (Ord a, Fractional a, Monad m) => AffineForm a -> AffineForm a -> m (AffineForm a)
 x ^/^ y = (x ^*^) =<< (recipAF y)
 
-(^**^) :: Num a => AffineForm a -> a -> AFM (AffineForm a)
-(AffineForm x xs xe) ^**^ y = undefined
+(^**) :: (Num a, Monad m) => AffineForm a -> a -> m (AffineForm a)
+(AffineForm x xs xe) ^** y = undefined
 
 -- Helper functions
 
@@ -93,3 +124,14 @@ zipWithZeros :: (Num a, Num b) => [a] -> [b] -> [(a,b)]
 zipWithZeros x y = take (max (length x) (length y)) $ zip infx infy
   where infx = x ++ repeat 0
         infy = y ++ repeat 0
+
+test :: IO ()
+test = do
+  let comp = do
+        xe <- newFromInterval (1...1)
+        ye <- newFromInterval (1...3)
+        op <- xe ^/^ ye
+        i <- interval op
+        return $ show i
+  undefined
+
