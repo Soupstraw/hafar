523e0270566da91446f7668556dd9793ca79ab92
Joosep Jääger <joosep.jaager@gmail.com>
2020-02-13 13:57:18 +0200

    Updated dependency version bounds. Renamed tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -139,29 +139,29 @@ correctnessPropBinary f g e x y = withMaxSuccess 5000 $ counterexample str res
 --         res = lhs `IA.contains` rhs
 --         str = "AA: " ++ (show lhs) ++ "\nIA: " ++ (show rhs)
 
-prop_addition :: EpsV Double -> AF s Double -> AF s Double -> Property
-prop_addition (EpsV e) x y = correctnessPropBinary (+) (+) e x y
+prop_sound_addition :: EpsV Double -> AF s Double -> AF s Double -> Property
+prop_sound_addition (EpsV e) x y = correctnessPropBinary (+) (+) e x y
 
-prop_subtraction :: EpsV Double -> AF s Double -> AF s Double -> Property
-prop_subtraction (EpsV e) x y = correctnessPropBinary (-) (-) e x y
+prop_sound_subtraction :: EpsV Double -> AF s Double -> AF s Double -> Property
+prop_sound_subtraction (EpsV e) x y = correctnessPropBinary (-) (-) e x y
 
-prop_multiplication :: EpsV Double -> AF s Double -> AF s Double -> Property
-prop_multiplication (EpsV e) x y = correctnessPropBinary (*) (*) e x y
+prop_sound_multiplication :: EpsV Double -> AF s Double -> AF s Double -> Property
+prop_sound_multiplication (EpsV e) x y = correctnessPropBinary (*) (*) e x y
 
-prop_power :: EpsV Double -> AF s Double -> SmallExponent Integer -> Property
-prop_power (EpsV e) x (SmallExponent n) = correctnessPropUnary (^n) (^n) e x
+prop_sound_power :: EpsV Double -> AF s Double -> SmallExponent Integer -> Property
+prop_sound_power (EpsV e) x (SmallExponent n) = correctnessPropUnary (^n) (^n) e x
 
-prop_recip :: EpsV Double -> ZerolessAF s Double -> Property
-prop_recip (EpsV e) (ZerolessAF x) = correctnessPropUnary recip recip e x
+prop_sound_recip :: EpsV Double -> ZerolessAF s Double -> Property
+prop_sound_recip (EpsV e) (ZerolessAF x) = correctnessPropUnary recip recip e x
 
-prop_log :: EpsV Double -> PositiveAF s Double -> Property
-prop_log (EpsV e) (PositiveAF x) = correctnessPropUnary log log e x
+prop_sound_log :: EpsV Double -> PositiveAF s Double -> Property
+prop_sound_log (EpsV e) (PositiveAF x) = correctnessPropUnary log log e x
 
-prop_exp :: EpsV Double -> SmallAF s Double -> Property
-prop_exp (EpsV e) (SmallAF x) = correctnessPropUnary exp exp e x
+prop_sound_exp :: EpsV Double -> SmallAF s Double -> Property
+prop_sound_exp (EpsV e) (SmallAF x) = correctnessPropUnary exp exp e x
 
-prop_abs :: EpsV Double -> AF s Double -> Property
-prop_abs (EpsV e) x = correctnessPropUnary abs abs e x
+prop_sound_abs :: EpsV Double -> AF s Double -> Property
+prop_sound_abs (EpsV e) x = correctnessPropUnary abs abs e x
 
 -- prop_sin :: EpsV Double -> AF s Double -> Property
 -- prop_sin (EpsV e) x = correctnessPropUnary sin sin e x
